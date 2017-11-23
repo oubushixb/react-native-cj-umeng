4c437a593bca21e9d937228fb417b305d7cf5004
chenjun <chenjun7878@qq.com>
2017-11-23 10:20:02 +0800

    新增umeng第三方封装.

---
+++ ios/ReactNativeCjUmeng/ReactNativeCjUmeng/ReactNativeCjUmeng.h
@@ -0,0 +1,13 @@
+//
+//  ReactNativeCjUmeng.h
+//  ReactNativeCjUmeng
+//
+//  Created by 陈俊 on 2017/11/23.
+//  Copyright © 2017年 陈俊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ReactNativeCjUmeng : NSObject
+
+@end
---
+++ ios/ReactNativeCjUmeng/ReactNativeCjUmeng/ReactNativeCjUmeng.m
@@ -0,0 +1,13 @@
+//
+//  ReactNativeCjUmeng.m
+//  ReactNativeCjUmeng
+//
+//  Created by 陈俊 on 2017/11/23.
+//  Copyright © 2017年 陈俊. All rights reserved.
+//
+
+#import "ReactNativeCjUmeng.h"
+
+@implementation ReactNativeCjUmeng
+
+@end
